80a998553276a329cc38416a2735e323d035d920
Saba Okriashvili <sabaaoqrish@gmail.com>
2023-03-31 14:32:46 +0400

    will check styles

---
+++ script.js
@@ -0,0 +1,17 @@
+let questions = [
+    {
+        question: "What is the smallest planet in our solar system?",
+        answers: ["Mercury", "Venus", "Earth", "Mars"],
+        correctAnswer: "Mercury"
+    },
+    {
+        question: "What is the tallest mountain in the world?",
+        answers: [ "Mount Kailash ","Mount Kilimanjaro", "Mount Everest", "Mount Fuji"],
+        correctAnswer: "Mount Everest"
+    },
+    {
+        question: "What temperature centigrade does water boil at?",
+        answers: [120, 100, 75, 155],
+        correctAnswer: 100
+    }
+]
